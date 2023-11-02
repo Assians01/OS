71c7cee08aa4a46b18119201d207f1c3ddf88813
Assassin01 <102428297+Assians01@users.noreply.github.com>
2023-11-02 22:09:29 +0530

    Create orphan.c

---
+++ orphan.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    pid_t child_pid = fork(); // Create a child process
+
+    if (child_pid < 0) {
+        perror("Fork failed");
+        exit(1);
+    } else if (child_pid == 0) {
+        // This code is executed by the child process
+        printf("Child process (PID %d) is running with parent(PID %d).\n", getpid(),getppid());
+        sleep(10); // Simulate some work
+        printf("Child process (PID %d) is done with parent(PID %d).\n", getpid(),getppid());
+        printf("Child process is done");
+    } else {
+        // This code is executed by the parent process
+        printf("Parent process (PID %d) is running with parent(PID %d).\n", getpid(),getppid());
+        // The parent process exits immediately, creating an orphan child process
+        printf("Parent process is done");
+        exit(0);
+    }
+
+    return 0;
+}
+
+================OUTPUT=======================
+Parent process (PID 1826) is running with parent(PID 1604).
+Child process (PID 1827) is running with parent(PID 1826).
+~$ Child process (PID 1827) is done with parent(PID 1).
