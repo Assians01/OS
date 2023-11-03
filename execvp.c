b5abd4f2100961da2cf37b7c36d209a614abc287
Assassin01 <102428297+Assians01@users.noreply.github.com>
2023-11-03 10:07:57 +0530

    Update execvp.c

---
+++ execvp.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+int main(){
+  char *command = "ls";
+  char *argument_list[] = {"ls","-l",NULL};
+  printf("Before calling execvp()\n");
+  int status_code = execvp(command,argument_list);
+  if(status_code == -1){
+     printf("Process did not terminate correctly\n");
+     exit(1);
+  }
+  printf("The line will not be printed if execvp() runs correctly\n");
+  return 0;
+}
